99c5155e0550045093cc2baf14d85ff13a787c16
Jean Lopes <jean.llopes@outlook.com>
2020-04-21 03:01:34 -0300

    Create a function

--- FunctionalCSharp/Program.cs
+++ FunctionalCSharp/Program.cs
@@ -44,16 +44,17 @@ namespace FunctionalCSharp
 
     internal class Program
     {
-        private static string BuildSelectBox(IDictionary<int, string> options, string id, bool includeUnknown) =>
-            new StringBuilder()
-                .AppendFormattedLine("<select id=\"{0}\" name=\"{0}\">", id)
-                .AppendLineWhen(
-                    () => includeUnknown,
-                    sb => sb.AppendLine("\t<option>Unknown</option>"))
-                .AppendSequence(options, (sb, opt) =>
-                    sb.AppendFormattedLine("\t<option value=\"{0}\">{1}</option>", opt.Key, opt.Value))
-                .AppendLine("</select>")
-                .ToString();
+        private static Func<IDictionary<int, string>, string> BuildSelectBox(string id, bool includeUnknown) =>
+            options =>
+                new StringBuilder()
+                    .AppendFormattedLine("<select id=\"{0}\" name=\"{0}\">", id)
+                    .AppendLineWhen(
+                        () => includeUnknown,
+                        sb => sb.AppendLine("\t<option>Unknown</option>"))
+                    .AppendSequence(options, (sb, opt) =>
+                        sb.AppendFormattedLine("\t<option value=\"{0}\">{1}</option>", opt.Key, opt.Value))
+                    .AppendLine("</select>")
+                    .ToString();
 
         private static void Main(string[] args)
         {
@@ -65,7 +66,7 @@ namespace FunctionalCSharp
                     .Split(new[] { Environment.NewLine, }, StringSplitOptions.RemoveEmptyEntries)
                     .Select((s, ix) => Tuple.Create(ix, s))
                     .ToDictionary(k => k.Item1, v => v.Item2)
-                    .Map(options => BuildSelectBox(options, "theDoctors", true))
+                    .Map(BuildSelectBox("theDoctors", true))
                     .Tee(Console.WriteLine);
 
             Console.ReadLine();
